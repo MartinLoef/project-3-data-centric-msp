4f4e4fd94e0f91622913f0fe507ba0620b9e5b02
Martin Loef <loefmartin@gmail.com>
2019-03-04 10:31:44 +0000

    added word document with user stories, mockups, technical description

--- config.py
+++ config.py
@@ -5,16 +5,13 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 secret_key = os.getenv('secret_key')
-print(secret_key)
 
 """ Explicit check for environment to use the production or dev database database """
 if os.getenv('C9_HOSTNAME'):
-    print('dev')
     mongodb = os.getenv('mongodb_dev')
-    print(mongodb)
 else:
     mongodb = os.getenv('mongodb_prod')
-    print(mongodb)
+    
 """ Flask base information """
 app = Flask(__name__)
 app.config["MONGO_URI"] = mongodb
